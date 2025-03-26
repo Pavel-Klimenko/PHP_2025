89bb4d2ea41f4308e25c30d4747f1e5243deca01
Pavel Klimenko <p.klimenko@itachsoft.com>
2025-03-26 16:52:30 +0300

    Убрал функцию отладки

--- public/index.php
+++ public/index.php
@@ -16,29 +16,3 @@ try {
 catch (Exception $e) {
      print_r($e->getMessage());
 }
-
-
-function pr_debug($var)
-{
-    //if ($_COOKIE['PHPSESSID'] == 'm66lhq4t81u3s2i4gjojdv2e23') {
-    $bt = debug_backtrace();
-    $bt = $bt[0];
-    ?>
-    <div style='font-size:9pt; color:#000; background:#fff; border:1px dashed #000;'>
-        <div style='padding:3px 5px; background:#99CCFF; font-weight:bold;'>File: <?= $bt["file"] ?>
-            [<?= $bt["line"] ?>]
-        </div>
-        <?
-        if ($var === 0) {
-            echo '<pre>пусто</pre>';
-            var_dump($var);
-        } else {
-            echo '<pre>';
-            print_r($var);
-            echo '</pre>';
-        }
-        ?>
-    </div>
-    <?
-    //}
-}
